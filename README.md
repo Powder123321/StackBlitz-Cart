5133f985ab7c0752ae048c71c91f10ae0faaaa0c
Powder123321 <100300541+Powder123321@users.noreply.github.com>
2024-02-12 03:39:50 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# kexihb-qu3ger
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Powder123321/kexihb-qu3ger)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css',
+})
+export class ProductDetailsComponent implements OnInit {
+  ngOnInit(): void {}
+  product: Product | undefined;
+  /*...*/
+}
